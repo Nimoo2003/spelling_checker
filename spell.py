dcfa01a736a0e950d54500d727e98ea798753930
Nimoo2003 <123817450+Nimoo2003@users.noreply.github.com>
2023-03-21 22:08:43 +0530

    Update spell.py

--- spell.py
+++ spell.py
@@ -2,11 +2,15 @@ from tkinter import*
 from textblob import TextBlob
 
 def check_spelling():
-    a=TextBlob(spell_check.get())
-    spell= Label(window, text="Correct is: ",font=("Arial",15,"bold"),bg="#f9e2e2",fg="black").place(x=224,y=290)
+    word=spell_check.get()
+    a=TextBlob(word)
+    right=str(a.correct())
 
-    correct_text= Label(window,text=str(a.correct()),font=("Arial",15,"bold"),bg="#f9e2e2",fg="black").place(x=350,y=290)
-    spell1.config(text=a.correct())
+    cs= Label(window, text="Correct is: ",font=("Arial",15,"bold"),bg="#f9e2e2",fg="black")
+    cs.place(x=224,y=290)
+
+    
+    spell1.config(text=right)
 
 
 
@@ -23,17 +27,16 @@ frame.place(x=100,y=50)
 heading= Label(frame, text="Spelling checker",bg="#ddbea9",fg="white",font=("Arial",30,"bold"))
 heading.place(x=100,y=5)
 
-frame1= Frame(window,height=40,width=200,bg='#ddbea9')
-frame1.place(x=240,y=150)
 
-
-spell_check = Entry(frame1, font=("Arial",15,"bold"),bg="white", fg="black")
-spell_check.pack()
+spell_check = Entry(window,justify="center",width=30, font=("Arial",15,"bold"),bg="white", fg="black")
+spell_check.pack(pady=150)
 spell_check.focus()
 
-button= Button(window, text="Check!!", font=("Arial",15,"bold"), bg="white", fg="black",command=check_spelling).place(x=300,y=210)
+button= Button(window, text="Check!!", font=("Arial",15,"bold"), bg="white", fg="black",command=check_spelling)
+button.place(x=300,y=210)
 
 spell1= Label(window,font=("Arial",15,"bold"),bg="#f9e2e2",fg="black")
 spell1.place(x=350,y=290)
 
 window.mainloop()
+
